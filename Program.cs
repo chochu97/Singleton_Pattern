65fcbcaf723d290e103d730144694d627457b9ed
nicok <nicokawaii97@gmail.com>
2024-07-09 19:28:24 -0300

    Final Changes

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using SingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario();
+
+            /*Console.WriteLine("Ingrese el nombre de usuario:  ");
+            usuario.Username = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la contraseña:  ");
+            usuario.Password = Console.ReadLine();*/
+
+            usuario.Username = "prueba";
+            usuario.Password = "prueba2";
+            
+            try
+            {
+                SessionManager.Login(usuario);
+
+                SessionManager u = SessionManager.GetInstance;
+
+                SessionManager.LogOut();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
+        }
+    }
+}
---
+++ SingleLibrary/SessionManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLibrary
+{
+    public class SessionManager
+    {
+        private static SessionManager _session;
+        private static object _lock = new Object();
+
+        public Usuario Usuario { get; set; }
+        public DateTime FechaInicio { get; set; }
+
+
+        public static SessionManager GetInstance
+        {
+            get
+            {
+                return _session;
+            }
+        }
+
+
+
+        public static void Login(Usuario user)
+        {
+            lock (_lock)
+            {
+                if (_session == null)
+                {
+                    _session = new SessionManager();
+                    _session.Usuario = user;
+                    _session.FechaInicio = DateTime.Now;
+                }
+                else
+                {
+                    throw new Exception("Sesion ya iniciada");
+                }
+            }
+
+        }
+
+        public static void LogOut()
+        {
+            lock (_lock)
+            {
+                if (_session != null)
+                {
+                    _session = null;
+                }
+                else
+                {
+                    throw new Exception("No hay sesion iniciada");
+                }
+            }
+
+        }
+        private SessionManager()
+        {
+
+        }
+    }
+}
